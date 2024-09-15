a40ba23472e075ccc10c6470f2db054dd151cc9f
Tony Lam <tony.lam@iki.fi>
2024-09-15 15:44:42 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import render_template
+import requests
+import requests_cache
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+# Get the API key from the environment variable
+api_key = os.getenv("api_key")
+
+# Set the proxy parameters
+proxy_params = {
+    "api_key": api_key,
+    "url": "https://www.alko.fi/tuotteet/101486/Jaloviina-muovipullo/",
+}
+
+# request cache to not call api all the time, expires after 1 hour
+requests_cache.install_cache("cache_name", expire_after=3600)
+
+# get request
+response = requests.get(
+    url="https://proxy.scrapeops.io/v1/",
+    params=urlencode(proxy_params),
+    timeout=120,
+)
+
+# use beautifulsoup to parse raw html
+soup = BeautifulSoup(response.content, "html.parser")
+# find class that houses the price
+price_container = soup.find(
+    class_="js-price-container price-wrapper price module-price"
+)
+# get aria label using slice
+hinta = str(price_container)[18:23]
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html", hinta=hinta)
